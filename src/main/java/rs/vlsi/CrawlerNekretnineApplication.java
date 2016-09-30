869e962146d5129c647170cb5fd19467345f7b89
Vladan Simic <vsimic@c16115.local>
2016-09-30 16:03:14 +0200

    initial commit

---
+++ src/main/java/rs/vlsi/CrawlerNekretnineApplication.java
@@ -0,0 +1,12 @@
+package rs.vlsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerNekretnineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerNekretnineApplication.class, args);
+	}
+}
